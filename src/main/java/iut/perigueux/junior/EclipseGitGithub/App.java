b50b3b000ad6bce888f505dc6e4e1c9ab128937a
Overdose <juniorturlet98@gmail.com>
2021-01-05 10:01:00 +0100

    commit 1

---
+++ src/main/java/iut/perigueux/junior/EclipseGitGithub/App.java
@@ -0,0 +1,13 @@
+package iut.perigueux.junior.EclipseGitGithub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello EPSI!" );
+    }
+}
